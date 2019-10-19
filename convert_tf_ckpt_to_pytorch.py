de6f0441d2e400c69963fec79f25f344a5776c71
Euphoria Yan <ysq58000@foxmail.com>
2019-10-19 16:10:46 +0800

    Update convert_tf_ckpt_to_pytorch_bin.

---
+++ convert_tf_ckpt_to_pytorch.py
@@ -0,0 +1,77 @@
+
+import os
+
+import tensorflow as tf
+import numpy as np
+import argparse
+
+from layers.bert_basic_model import *
+
+
+def tr(v):
+    # tensorflow weights to pytorch weights
+    if v.ndim == 4:
+        return np.ascontiguousarray(v.transpose(3, 2, 0, 1))
+    elif v.ndim == 2:
+        return np.ascontiguousarray(v.transpose())
+    return v
+
+
+def read_ckpt(ckpt):
+    # https://github.com/tensorflow/tensorflow/issues/1823
+    reader = tf.train.NewCheckpointReader(ckpt)
+    weights = {n: reader.get_tensor(n) for (n, _) in reader.get_variable_to_shape_map().iteritems()}
+    pyweights = {k: tr(v) for (k, v) in weights.items()}
+    return pyweights
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description="Converts ckpt weights to deepdish hdf5")
+    parser.add_argument("infile", type=str,
+                        help="Path to the ckpt.")
+    args = parser.parse_args()
+    state_dict = read_ckpt(args.infile)
+    bert_config = BertConfig.from_json_file("configs/bert_config.json")
+    model = BertModel(config=bert_config)
+
+    old_keys = []
+    new_keys = []
+    for key in state_dict.keys():
+        new_key = None
+        if 'gamma' in key:
+            new_key = key.replace('gamma', 'weight')
+        if 'beta' in key:
+            new_key = key.replace('beta', 'bias')
+        if new_key:
+            old_keys.append(key)
+            new_keys.append(new_key)
+    for old_key, new_key in zip(old_keys, new_keys):
+        state_dict[new_key] = state_dict.pop(old_key)
+
+    missing_keys = []
+    unexpected_keys = []
+    error_msgs = []
+    # copy state_dict so _load_from_state_dict can modify it
+    metadata = getattr(state_dict, '_metadata', None)
+    state_dict = state_dict.copy()
+    if metadata is not None:
+        state_dict._metadata = metadata
+
+    def load(module, prefix=''):
+        local_metadata = {} if metadata is None else metadata.get(prefix[:-1], {})
+        module._load_from_state_dict(
+            state_dict, prefix, local_metadata, True, missing_keys, unexpected_keys, error_msgs)
+        for name, child in module._modules.items():
+            if child is not None:
+                load(child, prefix + name + '.')
+
+
+    load(model, prefix='' if hasattr(model, 'bert') else 'bert.')
+    if len(missing_keys) > 0:
+        logger.info("Weights of {} not initialized from pretrained model: {}".format(
+            model.__class__.__name__, missing_keys))
+    if len(unexpected_keys) > 0:
+        logger.info("Weights from pretrained model not used in {}: {}".format(
+            model.__class__.__name__, unexpected_keys))
+
+    model.save_state_dict("output/pytorch.bin")
