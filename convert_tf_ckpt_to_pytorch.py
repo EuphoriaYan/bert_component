55c12bfca280f1e15031cc808d4c7f0fec67b343
Euphoria Yan <ysq58000@foxmail.com>
2019-10-29 02:14:38 +0800

    Update convert_tf_ckpt_to_pytorch.py

--- convert_tf_ckpt_to_pytorch.py
+++ convert_tf_ckpt_to_pytorch.py
@@ -1,6 +1,7 @@
 
 import os
 
+import re
 import tensorflow as tf
 import numpy as np
 import argparse
@@ -8,69 +9,70 @@ import argparse
 from layers.bert_basic_model import *
 
 
-def tr(v):
-    # tensorflow weights to pytorch weights
-    if v.ndim == 4:
-        return np.ascontiguousarray(v.transpose(3, 2, 0, 1))
-    elif v.ndim == 2:
-        return np.ascontiguousarray(v.transpose())
-    return v
+# Load tf checkpoints in a pytorch model.
+def load_tf_weights_in_bert(model, tf_checkpoint_path):
+    tf_path = os.path.abspath(tf_checkpoint_path)
+    logger.info("Converting TensorFlow checkpoint from {}".format(tf_path))
+    # Load weights from TF model
+    init_vars = tf.train.list_variables(tf_path)
+    names = []
+    arrays = []
+    for name, shape in init_vars:
+        logger.info("Loading TF weight {} with shape {}".format(name, shape))
+        array = tf.train.load_variable(tf_path, name)
+        names.append(name)
+        arrays.append(array)
 
-
-def read_ckpt(ckpt):
-    # https://github.com/tensorflow/tensorflow/issues/1823
-    reader = tf.train.NewCheckpointReader(ckpt)
-    weights = {n: reader.get_tensor(n) for (n, _) in reader.get_variable_to_shape_map().items()}
-    pyweights = {k: tr(v) for (k, v) in weights.items()}
-    return pyweights
+    for name, array in zip(names, arrays):
+        name = name.split('/')
+        # adam_v and adam_m are variables used in AdamWeightDecayOptimizer to calculated m and v
+        # which are not required for using pretrained model
+        if any(n in ["adam_v", "adam_m", "global_step", "cls"] for n in name):
+            logger.info("Skipping {}".format("/".join(name)))
+            continue
+        pointer = model
+        for m_name in name:
+            if re.fullmatch(r'[A-Za-z]+_\d+', m_name):
+                l = re.split(r'_(\d+)', m_name)
+            else:
+                l = [m_name]
+            if l[0] == 'kernel' or l[0] == 'gamma':
+                pointer = getattr(pointer, 'weight')
+            elif l[0] == 'output_bias' or l[0] == 'beta':
+                pointer = getattr(pointer, 'bias')
+            elif l[0] == 'output_weights':
+                pointer = getattr(pointer, 'weight')
+            elif l[0] == 'squad':
+                pointer = getattr(pointer, 'classifier')
+            else:
+                try:
+                    pointer = getattr(pointer, l[0])
+                except AttributeError:
+                    logger.info("Skipping {}".format("/".join(name)))
+                    continue
+            if len(l) >= 2:
+                num = int(l[1])
+                pointer = pointer[num]
+        if m_name[-11:] == '_embeddings':
+            pointer = getattr(pointer, 'weight')
+        elif m_name == 'kernel':
+            array = np.transpose(array)
+        try:
+            assert pointer.shape == array.shape
+        except AssertionError as e:
+            e.args += (pointer.shape, array.shape)
+            raise
+        logger.info("Initialize PyTorch weight {}".format(name))
+        pointer.data = torch.from_numpy(array)
+    return model
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="Converts tf bert ckpt weights to pytorch bin")
     parser.add_argument("infile", type=str, help="Path to the ckpt.")
+    parser.add_argument("outfile", type=str, help="Path to the pytorch dump.")
     args = parser.parse_args()
-    state_dict = read_ckpt(args.infile)
     bert_config = BertConfig.from_json_file("configs/bert_config.json")
-    model = BertModel(config=bert_config)
-
-    old_keys = []
-    new_keys = []
-    for key in state_dict.keys():
-        new_key = None
-        if 'gamma' in key:
-            new_key = key.replace('gamma', 'weight')
-        if 'beta' in key:
-            new_key = key.replace('beta', 'bias')
-        if new_key:
-            old_keys.append(key)
-            new_keys.append(new_key)
-    for old_key, new_key in zip(old_keys, new_keys):
-        state_dict[new_key] = state_dict.pop(old_key)
-
-    missing_keys = []
-    unexpected_keys = []
-    error_msgs = []
-    # copy state_dict so _load_from_state_dict can modify it
-    metadata = getattr(state_dict, '_metadata', None)
-    state_dict = state_dict.copy()
-    if metadata is not None:
-        state_dict._metadata = metadata
-
-    def load(module, prefix=''):
-        local_metadata = {} if metadata is None else metadata.get(prefix[:-1], {})
-        module._load_from_state_dict(
-            state_dict, prefix, local_metadata, True, missing_keys, unexpected_keys, error_msgs)
-        for name, child in module._modules.items():
-            if child is not None:
-                load(child, prefix + name + '.')
-
-
-    load(model, prefix='' if hasattr(model, 'bert') else 'bert.')
-    if len(missing_keys) > 0:
-        logger.info("Weights of {} not initialized from pretrained model: {}".format(
-            model.__class__.__name__, missing_keys))
-    if len(unexpected_keys) > 0:
-        logger.info("Weights from pretrained model not used in {}: {}".format(
-            model.__class__.__name__, unexpected_keys))
-
-    torch.save(model.state_dict(), "output/pytorch_model.bin")
+    model = BertModel(bert_config)
+    load_tf_weights_in_bert(model, args.infile)
+    torch.save(model.state_dict(), args.outfile)
