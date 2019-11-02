04fe83f4f58a5194379799b3d16513102c4d0505
Euphoria Yan <ysq58000@foxmail.com>
2019-11-02 20:13:10 +0800

    Update code.

--- create_pretraining_data.sh
+++ create_pretraining_data.sh
@@ -1,9 +1,11 @@
 # -*- coding: utf-8 -*-
+input_file=./简体/集藏
 output_file=output/jizang_data
 vocab_file=vocab2.txt
 
 
 python create_pretraining_data.py \
+--input_file ${input_file} \
 --output_file ${output_file} \
 --vocab_file ${vocab_file} \
 --do_whole_word_mask True
