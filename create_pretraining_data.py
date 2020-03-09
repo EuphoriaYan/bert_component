190e04a6be572f6843954835fb12f0886f544b2b
Euphoria Yan <ysq58000@foxmail.com>
2020-03-09 12:08:40 +0800

    Update scripts.

--- create_pretraining_data.py
+++ create_pretraining_data.py
@@ -479,7 +479,7 @@ def main(_):
 
 
 if __name__ == "__main__":
-    flags.mark_flag_as_required("iutput_file")
+    flags.mark_flag_as_required("input_file")
     flags.mark_flag_as_required("output_file")
     flags.mark_flag_as_required("vocab_file")
     tf.app.run()
