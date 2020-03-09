4464d3415e75d95966c4c71770727b3678c08fcf
Euphoria Yan <ysq58000@foxmail.com>
2020-03-09 12:28:01 +0800

    Update scripts.

---
+++ create_pretraining_data_leishu.sh
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+input_file=corpus/cleaned/leishu.txt
+output_file=tfrecords/leishu.tfrecord1
+vocab_file=vocab2.txt
+
+
+python create_pretraining_data.py \
+--input_file ${input_file} \
+--output_file ${output_file} \
+--vocab_file ${vocab_file} \
+--do_whole_word_mask True
