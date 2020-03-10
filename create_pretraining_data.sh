27e9b9529d4d7aa55e01fe7e01b2b0348db5a066
Euphoria Yan <ysq58000@foxmail.com>
2020-03-10 17:33:54 +0800

    Update scripts.

--- create_pretraining_data.sh
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-nohup sh create_pretraining_data_dz.sh > output/dz.log 2>&1 &
-nohup sh create_pretraining_data_dzj.sh > output/dzj.log 2>&1 &
-nohup sh create_pretraining_data_gujin.sh > output/gujin.log 2>&1 &
-nohup sh create_pretraining_data_leishu.sh > output/leishu.log 2>&1 &
-nohup sh create_pretraining_data_shibu.sh > output/shibu.log 2>&1 &
-nohup sh create_pretraining_data_yiji.sh > output/yiji.log 2>&1 &
-nohup sh create_pretraining_data_zibu.sh > output/zibu.log 2>&1 &
-nohup sh create_pretraining_data_zongji.sh > output/zongji.log 2>&1 &
